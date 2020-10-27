f8531ee8a63e6036f1816f00fce41e2d8e8fd854
MarcioFCMartins <marciomartinsred@gmail.com>
2020-10-27 16:03:58 +0000

    Updated project folder structure and uploaded templates

--- R/create_structure.R
+++ R/create_structure.R
@@ -8,20 +8,26 @@
 
 # Create folder structure
 create_structure <- function() {
-  folders <- c("./1_documentation",
-               "./2_planning_recourses",
-               "./3_data",
-               "./3_data/raw",
-               "./4_code",
-               "./5_outputs",
-               "./6_manuscripts",
-               "./z_photo_backups")
+  folders <- c("./01_documentation",
+               "./01_documentation/01_planning_recourses",
+               "./02_data",
+               "./02_data/raw",
+               "./02_data/z_photo_backups",
+               "./03_code",
+               "./04_outputs",
+               "./05_analusis",
+               "./05_outputs/manuscripts",
+               "./05_outputs/reports"               )
 
   lapply(folders,
          function(path) if(!dir.exists(path)){
            dir.create(path, FALSE)})
 
   download.file(
-    "https://github.com/MarcioFCMartins/MMartins/raw/master/project_structure/3_data/clean_data.xlsx",
-    "./3_data/clean_data.xlsx")
+    "https://github.com/MarcioFCMartins/MMartins/raw/master/workflow_files/clean_data.xlsx",
+    "./02_data/clean_data.xlsx")
+  download.file(
+    "https://github.com/MarcioFCMartins/MMartins/raw/master/workflow_files/clean_data.xlsx",
+    "./05_outputs/reports/word_document2-template.docx"
+  )
 }
