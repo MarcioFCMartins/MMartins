db410eb66c1a826b8149f3b38b5025114bc2feae
MarcioFCMartins <marciomartinsred@gmail.com>
2020-10-27 16:13:56 +0000

    Update project structure

--- R/create_structure.R
+++ R/create_structure.R
@@ -15,19 +15,19 @@ create_structure <- function() {
                "./02_data/z_photo_backups",
                "./03_code",
                "./04_outputs",
-               "./05_analusis",
-               "./05_outputs/manuscripts",
-               "./05_outputs/reports"               )
+               "./05_analysis",
+               "./05_analysis/manuscripts",
+               "./05_analysis/reports"               )
 
   lapply(folders,
          function(path) if(!dir.exists(path)){
            dir.create(path, FALSE)})
 
   download.file(
-    "https://github.com/MarcioFCMartins/MMartins/raw/master/workflow_files/clean_data.xlsx",
+    "https://github.com/MarcioFCMartins/MMartins/blob/master/workflow_files/clean_data.xlsx",
     "./02_data/clean_data.xlsx")
   download.file(
-    "https://github.com/MarcioFCMartins/MMartins/raw/master/workflow_files/clean_data.xlsx",
-    "./05_outputs/reports/word_document2-template.docx"
+    "https://github.com/MarcioFCMartins/MMartins/blob/master/workflow_files/word_document2-template.docx",
+    "./05_analysis/reports/word_document2-template.docx"
   )
 }
