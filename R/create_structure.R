826eadef1feb0bf4b2c040a56843df5e4b0e3ff8
MarcioFCMartins <marciomartinsred@gmail.com>
2021-01-12 19:29:20 +0000

    Added an Rproj template that defines folder structure and creates README for research projects

--- R/create_structure.R
+++ R/create_structure.R
@@ -1,33 +1,50 @@
-#' Create folder structure
+#' This function is called by Rstudio when a Research project rproj template is created
+#' Defines folder structure and creates a README
 #'
-#' This function creates folders inside your project folder to help organize your files
-#' @examples
-#' create_structure()
-#' @export
 
-
-# Create folder structure
-create_structure <- function() {
-  folders <- c("./01_documentation",
-               "./01_documentation/01_planning_recourses",
-               "./02_data",
-               "./02_data/raw",
-               "./02_data/z_photo_backups",
-               "./03_code",
-               "./04_outputs",
-               "./05_analysis",
-               "./05_analysis/manuscripts",
-               "./05_analysis/reports"               )
+research_project <- function(path, ...) {
+  # Create folders
+  folders <- paste0(
+    path,
+    c("",
+      "/01-documentation",
+      "/01-documentation/planning-recourses",
+      "/02-data",
+      "/02-data/raw",
+      "/02-data/clean",
+      "/02-data/photo-backups",
+      "/03-scripts",
+      "/04-outputs",
+      "/05-analysis",
+      "/05-analysis/manuscripts",
+      "/05-analysis/reports")
+  )
 
   lapply(folders,
          function(path) if(!dir.exists(path)){
            dir.create(path, FALSE)})
 
-  download.file(
-    "https://github.com/MarcioFCMartins/MMartins/blob/master/workflow_files/clean_data.xlsx",
-    "./02_data/clean_data.xlsx")
-  download.file(
-    "https://github.com/MarcioFCMartins/MMartins/blob/master/workflow_files/word_document2-template.docx",
-    "./05_analysis/reports/word_document2-template.docx"
+  writeLines(
+    c("# Metadata", "What is this project?", "Who is involved?",
+      "When did it start?", "", "# Journal", "What actions were taken to achieve the objectives and when?",
+      "", "# Folder structure", "", "project-name", "|", "+-- project-name.Rproj",
+      "|", "+-- 01-documentation\t\t\tLegal, financial and logistic documents\t  ",
+      "|\t|", "|\t`-- planning-resources\t\t", "|", "+-- 02-data\t\t\t\t\t\t",
+      "|\t|", "|\t+-- raw-data\t\t\t\tCollected data, as written/collected",
+      "|\t|", "|\t+-- clean-data              Processed data for analysis",
+      "|\t|\t|", "|\t|\t+-- *.csv\t\t\t\t\tData files ready for analysis",
+      "|\t|\t`-- *-README.txt\t\t\tData dictionary for data files",
+      "|\t|", "|\t`-- photo_backups\t\t\tPhotographic backup of field tables or others",
+      "|", "+-- 03-scripts\t\t\t\t\tScripts used for analysis ", "|\t|",
+      "|\t+-- 0-process-data.R\t\t\tProcess raw data into clean", "|\t`-- k-*.R\t\t\t\t\t\tScripts for other processes",
+      "|", "+-- 04-outputs\t\t\t\t\tOutputs of scripts - figures, tables, models, etc",
+      "|", "+-- 05-analysis\t\t\t\t\t", "|\t|", "|\t+-- reports\t\t\t\t\t",
+      "|\t|", "|\t+-- manuscript", "|\t|\t|", "|\t|\t`-- biblio.bib \t\t\t\tBibliography used in manuscript "),
+    paste0(path, "/README.txt")
   )
+
+  # Create README.txt
+
+
+
 }
--- R/df_to_xlsx.R
+++ R/df_to_xlsx.R
@@ -11,26 +11,26 @@
 #' df_to_xlsx(excel_file = "./excel.xlsx", sheet_name = "information", df = info_df, merge_cols = c("Continent", "Country"))
 #' @export
 
-df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_cols = NULL, nested_merge = TRUE){
-
-  if(file.exists(excel_file)){
-    wb <- openxlsx::loadWorkbook(excel_file)             # Load excel file if it exists
+df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_cols = NULL, nested_merge = TRUE) {
+  if (file.exists(excel_file)) {
+    wb <- openxlsx::loadWorkbook(excel_file) # Load excel file if it exists
   } else {
     input <- readline("This file does not exist, create a new one? (y/n)")
 
-    if(tolower(input) == "y"){
+    if (tolower(input) == "y") {
       wb <- openxlsx::createWorkbook()
       message("New excel file was created")
-
     } else {
       return(message("Please provide the correct filename"))
     }
   }
 
   # Check some basic conditions before running function
-  stopifnot(is.character(sheet_name),# Was a sheet name provided
-            exists(deparse(substitute(df))), # Was an object provided to df
-            merge_cols %in% colnames(df)) # Are merge columns present in df
+  stopifnot(
+    is.character(sheet_name), # Was a sheet name provided
+    exists(deparse(substitute(df))), # Was an object provided to df
+    merge_cols %in% colnames(df)
+  ) # Are merge columns present in df
 
   # Set minimum column width for when auto column sizing is done
   options("openxlsx.minWidth" = 6)
@@ -39,55 +39,64 @@ df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_
   # and thick border at the bottom
   header_style <- openxlsx::createStyle(
     textDecoration = "bold",
-    border         = c("top", "bottom"),
-    borderStyle    = c("thin", "thick"),
-    borderColour   = c("black", "black"),
-    fgFill         = c("#ffe699"),
-    wrapText       = TRUE,
-    halign         = "center")
+    border = c("top", "bottom"),
+    borderStyle = c("thin", "thick"),
+    borderColour = c("black", "black"),
+    fgFill = c("#ffe699"),
+    wrapText = TRUE,
+    halign = "center"
+  )
 
 
   # Check if provided sheet name exists - remove it if it does
-  if(sheet_name %in% names(wb)){
+  if (sheet_name %in% names(wb)) {
     openxlsx::removeWorksheet(
       wb,
-      sheet = sheet_name)
+      sheet = sheet_name
+    )
 
-    message("Your workbook already had a sheet named ", sheet_name,
-            ".\nThis sheet has been replaced!")
+    message(
+      "Your workbook already had a sheet named ", sheet_name,
+      ".\nThis sheet has been replaced!"
+    )
   }
 
   # Create a new sheet in the workbook
   openxlsx::addWorksheet(wb, sheetName = sheet_name)
 
   # Write data.frame to created sheet - with header style
-  openxlsx::writeData(wb    = wb,
-                      sheet = sheet_name,
-                      x     = df,
-                      headerStyle = header_style)
+  openxlsx::writeData(
+    wb = wb,
+    sheet = sheet_name,
+    x = df,
+    headerStyle = header_style
+  )
 
   # Auto size columns to fit all the text
-  if(auto_size_cols){
+  if (auto_size_cols) {
     openxlsx::setColWidths(wb,
-                           sheet = sheet_name,
-                          cols  = 1:ncol(df),
-                           widths = "auto")
-    }
+      sheet = sheet_name,
+      cols = 1:ncol(df),
+      widths = "auto"
+    )
+  }
 
   # Merge cells of specified columns when they are equal
-  if(!is.null(merge_cols)) {
+  if (!is.null(merge_cols)) {
     # Matrix where break points are represented as 1's
-    break_matrix <- matrix(nrow = nrow(df)+1,
-                           ncol = length(merge_cols))
+    break_matrix <- matrix(
+      nrow = nrow(df) + 1,
+      ncol = length(merge_cols)
+    )
     break_matrix[] <- 0
     # Loop over columns - create break points for each individual column
     for (col_index in 1:length(merge_cols)) {
       col_name <- merge_cols[col_index]
       # Get vector of elements in column
-      elements  <- df[[col_name]]
+      elements <- df[[col_name]]
 
       # Initiate indices
-      first <- 1    # First element in group
+      first <- 1 # First element in group
       # Loop over elements in column
       for (row_index in 1:length(elements)) {
         if (elements[row_index] != elements[first]) {
@@ -99,45 +108,52 @@ df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_
       }
     }
 
-    if(nested_merge){
+    if (nested_merge) {
       # Propagate breaks from previous columns into next one
-      for (col_index in 2:ncol(break_matrix)){
-        for(row_index in 1:nrow(break_matrix)){
-          break_matrix[row_index, col_index] <- sum(break_matrix[row_index,1:col_index])
+      for (col_index in 2:ncol(break_matrix)) {
+        for (row_index in 1:nrow(break_matrix)) {
+          break_matrix[row_index, col_index] <- sum(break_matrix[row_index, 1:col_index])
         }
       }
     }
 
     # Make first and last breaks always breaks
-    break_matrix[1,] <- 1
-    break_matrix[nrow(break_matrix),] <- 1
+    break_matrix[1, ] <- 1
+    break_matrix[nrow(break_matrix), ] <- 1
 
     break_matrix[break_matrix > 0] <- 1
 
 
     # Convert matrix into a list and apply merge function
-    for (i in 1:ncol(break_matrix)){
+    for (i in 1:ncol(break_matrix)) {
       col_index <- which(colnames(df) == merge_cols[i])
-      break_indices <- (which(break_matrix[,col_index] == 1))+1
-      merge_matrix <- matrix(nrow = length(break_indices)-1,
-                             ncol = 2)
-      for (break_i in 1:(length(break_indices)-1)){
-        merge_matrix[break_i,1] <- break_indices[break_i]
-        merge_matrix[break_i,2] <- break_indices[break_i+1] -1
+      break_indices <- (which(break_matrix[, col_index] == 1)) + 1
+      merge_matrix <- matrix(
+        nrow = length(break_indices) - 1,
+        ncol = 2
+      )
+      for (break_i in 1:(length(break_indices) - 1)) {
+        merge_matrix[break_i, 1] <- break_indices[break_i]
+        merge_matrix[break_i, 2] <- break_indices[break_i + 1] - 1
       }
-      merge_matrix <- merge_matrix[(merge_matrix[,2] - merge_matrix[,1]) > 0,]
+      merge_matrix <- merge_matrix[(merge_matrix[, 2] - merge_matrix[, 1]) > 0, ]
       merge_matrix <- as.vector(t(merge_matrix))
-      indices <- split(merge_matrix, ceiling(seq_along(merge_matrix)/2))
+      indices <- split(merge_matrix, ceiling(seq_along(merge_matrix) / 2))
 
-      lapply(X = indices,
-             FUN = function(x) openxlsx::mergeCells(wb = wb,
-                                          sheet = sheet_name,
-                                          cols = col_index,
-                                          rows = x))
+      lapply(
+        X = indices,
+        FUN = function(x) {
+          openxlsx::mergeCells(
+            wb = wb,
+            sheet = sheet_name,
+            cols = col_index,
+            rows = x
+          )
+        }
+      )
     }
-
   }
 
-  #Save final excel workbook
+  # Save final excel workbook
   openxlsx::saveWorkbook(wb, excel_file, overwrite = TRUE)
 }
