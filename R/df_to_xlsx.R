80c459aec065a0808a9cac0150c95f1d67a687d2
MarcioFCMartins <marciomartinsred@gmail.com>
2023-11-17 12:13:17 +0000

    Updated df_to_xlsx
    Now uses the new `openxlsx2` package.
    The column merging code was simplified and should now be faster

--- R/df_to_xlsx.R
+++ R/df_to_xlsx.R
@@ -7,18 +7,20 @@
 #' @param auto_size_cols Should column width be changed based on their contents? TRUE or FALSE, defaults to TRUE
 #' @param merge_cols Names of columns that should be merged vertically when they have the same value. Default is no merging, expects a character vector
 #' @param nested_merge Should merging be done with columns on the right being nested inside the ones on the left? TRUE or FALSE
+#' @return Excel file with data.frame added
+#'
 #' @examples
 #' df_to_xlsx(excel_file = "./excel.xlsx", sheet_name = "information", df = info_df, merge_cols = c("Continent", "Country"))
 #' @export
 
 df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_cols = NULL, nested_merge = TRUE) {
   if (file.exists(excel_file)) {
-    wb <- openxlsx::loadWorkbook(excel_file) # Load excel file if it exists
+    wb <- openxlsx2::wb_load(excel_file) # Load excel file if it exists
   } else {
     input <- readline("This file does not exist, create a new one? (y/n)")
 
     if (tolower(input) == "y") {
-      wb <- openxlsx::createWorkbook()
+      wb <- openxlsx2::wb_workbook()
       message("New excel file was created")
     } else {
       return(message("Please provide the correct filename"))
@@ -27,133 +29,123 @@ df_to_xlsx <- function(excel_file, sheet_name, df, auto_size_cols = TRUE, merge_
 
   # Check some basic conditions before running function
   stopifnot(
-    is.character(sheet_name), # Was a sheet name provided
+    is.character(sheet_name),        # Was a sheet name provided
     exists(deparse(substitute(df))), # Was an object provided to df
-    merge_cols %in% colnames(df)
-  ) # Are merge columns present in df
-
-  # Set minimum column width for when auto column sizing is done
-  options("openxlsx.minWidth" = 6)
-
-  # Style for column header - bold, centered, wrapped, yellow background
-  # and thick border at the bottom
-  header_style <- openxlsx::createStyle(
-    textDecoration = "bold",
-    border = c("top", "bottom"),
-    borderStyle = c("thin", "thick"),
-    borderColour = c("black", "black"),
-    fgFill = c("#ffe699"),
-    wrapText = TRUE,
-    halign = "center"
+    merge_cols %in% colnames(df)     # Are merge columns present in df
   )
 
+  # Set minimum column width for when auto column sizing is done
+  options("openxlsx2.minWidth" = 6)
 
   # Check if provided sheet name exists - remove it if it does
-  if (sheet_name %in% names(wb)) {
-    openxlsx::removeWorksheet(
-      wb,
-      sheet = sheet_name
-    )
+  if (sheet_name %in% openxlsx2::wb_get_sheet_names(wb)) {
+    # Save current sheet order
+    sheet_order <- openxlsx2::wb_get_sheet_names(wb)
+    wb <- openxlsx2::wb_remove_worksheet(wb, sheet = sheet_name)
+    wb <- openxlsx2::wb_add_worksheet(wb, sheet = sheet_name)
+    # Re-order sheets as original
+    new_sheet_order <- match(openxlsx2::wb_get_sheet_names(wb), sheet_order)
+    wb <- wb_set_order(wb, new_sheet_order)
 
     message(
       "Your workbook already had a sheet named ", sheet_name,
       ".\nThis sheet has been replaced!"
     )
+  } else { # If not, add a sheet
+    wb <- openxlsx2::wb_add_worksheet(wb, sheet = sheet_name)
   }
 
-  # Create a new sheet in the workbook
-  openxlsx::addWorksheet(wb, sheetName = sheet_name)
-
-  # Write data.frame to created sheet - with header style
-  openxlsx::writeData(
+  # Add data.frame to excel sheet
+  wb <- openxlsx2::wb_add_data(
     wb = wb,
-    sheet = sheet_name,
     x = df,
-    headerStyle = header_style
-  )
+    sheet = sheet_name
+    )
 
   # Auto size columns to fit all the text
   if (auto_size_cols) {
-    openxlsx::setColWidths(wb,
+    wb <- openxlsx2::wb_set_col_widths(
+      wb,
       sheet = sheet_name,
       cols = 1:ncol(df),
       widths = "auto"
     )
   }
 
+
+  # Style for column header - bold, centered, wrapped, yellow background
+  # and thick border at the bottom
+  header_dims <- wb_dims(x = df, select = "col_names")
+
+  wb <- wb |>
+    openxlsx2::wb_add_border(
+      dims = header_dims,
+      top_color = wb_color(hex = "#FF000000"),
+      top_style = "thin",
+      bottom_color = wb_color(hex = "#FF000000"),
+      bottom_style = "thick")|>
+    openxlsx2::wb_add_fill(dims = header_dims, color = wb_color(hex = "#ffbb70")) |>
+    openxlsx2::wb_add_font(dims = header_dims, bold = TRUE) |>
+    openxlsx2::wb_add_cell_style(
+      dims = header_dims, horizontal = "center", vertical = "center")
+
+
+
   # Merge cells of specified columns when they are equal
   if (!is.null(merge_cols)) {
-    # Matrix where break points are represented as 1's
-    break_matrix <- matrix(
-      nrow = nrow(df) + 1,
-      ncol = length(merge_cols)
-    )
-    break_matrix[] <- 0
-    # Loop over columns - create break points for each individual column
+    merge_ranges <- list()
+    # Identify breakpoints for all columns
     for (col_index in 1:length(merge_cols)) {
       col_name <- merge_cols[col_index]
       # Get vector of elements in column
       elements <- df[[col_name]]
+      # Get run lengths of elements
+      run_lengths <- cumsum(rle(elements)$lengths)
+      # First merge group starts at 2. Then, each group starts at the end
+      # of the previous one, shifted by +1 added because of the header row and
+      # +1 again to start after the last group
+      start_merge_indices <- c(2, run_lengths[-length(run_lengths)] + 2)
+      # End of merge groups, shifted by +1 for header
+      end_merge_indices <- c(run_lengths + 1)
 
-      # Initiate indices
-      first <- 1 # First element in group
-      # Loop over elements in column
-      for (row_index in 1:length(elements)) {
-        if (elements[row_index] != elements[first]) {
-          # If 2 sequential elements are diferent, label them as break points
-          break_matrix[row_index, col_index] <- 1
-          # Update start of next group
-          first <- row_index
-        }
+      if (isTRUE(nested_merge) & col_index > 1) {
+        # If nested merge is TRUE, merge ranges from previous column are
+        # added to this column
+        prev_col_ranges <- merge_ranges[[col_index - 1]]
+        # Combine start merge ranges and remove duplicates
+        start_merge_indices <- unique(
+          c(start_merge_indices, prev_col_ranges[, 1])
+        )
+        start_merge_indices <- start_merge_indices[order(start_merge_indices)]
+        # Combine end merge ranges and remove duplicates
+        end_merge_indices <- unique(
+          c(end_merge_indices, prev_col_ranges[, 2])
+        )
+        end_merge_indices <- end_merge_indices[order(end_merge_indices)]
       }
+
+      merge_ranges[[col_index]] <- cbind(start_merge_indices, end_merge_indices)
     }
 
-    if (nested_merge) {
-      # Propagate breaks from previous columns into next one
-      for (col_index in 2:ncol(break_matrix)) {
-        for (row_index in 1:nrow(break_matrix)) {
-          break_matrix[row_index, col_index] <- sum(break_matrix[row_index, 1:col_index])
-        }
+    # Apply merge function to worksheet based on merge ranges
+    for (col_index in 1:length(merge_cols)) {
+      indices <- merge_ranges[[col_index]]
+      col_name <- merge_cols[col_index]
+      col_pos <- which(names(df) == col_name)
+
+      for(i in 1:nrow(indices)){
+        index <- indices[i, ]
+        wb <- wb_merge_cells(
+          wb = wb,
+          sheet = sheet_name,
+          dims = openxlsx2::wb_dims(
+            cols = col_pos,
+            rows = c(index[1]:index[2]))
+        )
       }
-    }
-
-    # Make first and last breaks always breaks
-    break_matrix[1, ] <- 1
-    break_matrix[nrow(break_matrix), ] <- 1
-
-    break_matrix[break_matrix > 0] <- 1
-
-
-    # Convert matrix into a list and apply merge function
-    for (i in 1:ncol(break_matrix)) {
-      col_index <- which(colnames(df) == merge_cols[i])
-      break_indices <- (which(break_matrix[, col_index] == 1)) + 1
-      merge_matrix <- matrix(
-        nrow = length(break_indices) - 1,
-        ncol = 2
-      )
-      for (break_i in 1:(length(break_indices) - 1)) {
-        merge_matrix[break_i, 1] <- break_indices[break_i]
-        merge_matrix[break_i, 2] <- break_indices[break_i + 1] - 1
-      }
-      merge_matrix <- merge_matrix[(merge_matrix[, 2] - merge_matrix[, 1]) > 0, ]
-      merge_matrix <- as.vector(t(merge_matrix))
-      indices <- split(merge_matrix, ceiling(seq_along(merge_matrix) / 2))
-
-      lapply(
-        X = indices,
-        FUN = function(x) {
-          openxlsx::mergeCells(
-            wb = wb,
-            sheet = sheet_name,
-            cols = col_index,
-            rows = x
-          )
-        }
-      )
     }
   }
 
   # Save final excel workbook
-  openxlsx::saveWorkbook(wb, excel_file, overwrite = TRUE)
+  openxlsx2::wb_save(wb, excel_file, overwrite = TRUE)
 }
